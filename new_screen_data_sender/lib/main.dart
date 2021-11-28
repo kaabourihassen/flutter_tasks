e224907a5a83a58315b8af818f2ccd0f53f9062a
Hassen Kaabouri <kaabouri.hassen704@gmail.com>
2021-11-28 13:45:41 +0100

    create todo screen to display list

--- new_screen_data_sender/lib/main.dart
+++ new_screen_data_sender/lib/main.dart
@@ -80,6 +80,39 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
 class Todo {
   final String title;
   final String description;
